014915d72a30f00e00a1eb74cf10e246ce5e0d4e
qortn4925 <qortn4925@naver.com>
2024-08-14 15:42:16 +0900

    p44 연습문제 바이트

---
+++ src/ch02/lecture/p02type/C01Type.java
@@ -0,0 +1,19 @@
+package ch02.lecture.p02type;
+
+public class C01Type {
+    public static void main(String[] args) {
+
+        //java data type 2가지
+        //1.primitive type 8가지
+        // byte:1 바이트 정수   short: 2바이트 정수
+        //  int : 4 바이트     long :8 바이트
+        // float: 4바이트 실수 , dobule : 8 바이트 실수
+        // boolean : 1 바이트 논리
+        // char: 2바이트 문자
+
+        //2.reference type
+        // >> primitive 타이블 제외한 나머지\
+        int v1 = 2147483647;
+        System.out.println("v1+1 = " + (v1+1));
+    }
+}
---
+++ src/ch02/sec02/ByteExample.java
@@ -0,0 +1,16 @@
+package ch02.sec02;
+
+public class ByteExample {
+    public static void main(String[] args) {
+        byte var1 = -128;
+        byte var2 = -30;
+       // byte var3 =128;
+        byte var4 =127;
+
+        System.out.println("var1 = " + var1);
+        System.out.println("var2 = " + var2);
+//        System.out.println("var3 = " + var3);
+
+        System.out.println("var4 = " + var4);
+    }
+}
