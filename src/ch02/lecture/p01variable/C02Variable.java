1aff7b64c2cfe83c5c2890a18ce953554f4449b5
qortn4925 <qortn4925@naver.com>
2024-08-14 14:58:03 +0900

    변수명 작성 관습

---
+++ src/ch02/lecture/p01variable/C02Variable.java
@@ -0,0 +1,20 @@
+package ch02.lecture.p01variable;
+
+public class C02Variable {
+    public static void main(String[] args) {
+        //변수명 작성 관습
+        //소문자
+        int age;
+        int Age;
+        int AGE;
+        int aGE;
+
+        //여러 단어 조합시 각 단어 앞만 대문자
+        int numberOfStudent;
+        int number_of_stduents;  // snake_case( python 관습)
+
+        // lowerCamelCase > 낙타 혹처럼 생겼다해서 , 자바 변수병 관습
+        // upperCamelCASE   자바 클래스 관습
+        // kebab-case (기타 등등)
+    }
+}
