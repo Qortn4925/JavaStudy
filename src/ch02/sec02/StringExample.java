8b5b63281926e722de45d8b4bdb47b087e7f6078
qortn4925 <qortn4925@naver.com>
2024-08-19 09:26:35 +0900

    p52 StringExample

---
+++ src/ch02/sec02/StringExample.java
@@ -0,0 +1,16 @@
+package ch02.sec02;
+
+public class StringExample {
+    public static void main(String[] args) {
+        String name = "홍길동";
+        String job = "프로그래ㅓㅁ";
+        System.out.println("name = " + name);
+        System.out.println("job = " + job);
+
+        String str = "나는 \"자바를\" 배웁니다";
+        System.out.println("str = " + str);
+        str = "번호\t이름\t직업";
+        System.out.println("str = " + str);
+
+    }
+}
