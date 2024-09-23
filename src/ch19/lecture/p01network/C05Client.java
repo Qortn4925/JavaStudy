042f75cf77b3601504e8016a4860a54eb1013328
qortn4925 <qortn4925@naver.com>
2024-09-23 12:31:02 +0900

    유사 채팅창 만들기 , PrintStream

--- src/ch19/lecture/p01network/C05Client.java
+++ src/ch19/lecture/p01network/C05Client.java
@@ -1,8 +1,44 @@
 package ch19.lecture.p01network;
 
-public class C05Client {
-    public static void main(String[] args) {
+import java.io.BufferedOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Scanner;
 
-        
+public class C05Client {
+    public static void main(String[] args) throws Exception {
+
+        // 키보드 입력을 server 로 보내기
+        //키보드로 부터 입력 받고 ,
+
+        Socket socket = new Socket("172.30.1.84", 9999);
+        System.out.println(" 연결됨 !!");
+
+        OutputStream os = socket.getOutputStream();
+        BufferedOutputStream bos = new BufferedOutputStream(os);
+        PrintStream ps = new PrintStream(bos);
+
+
+        // socket 은  프로그램 기준 나가니까  output
+        Scanner sc = new Scanner(System.in);
+        try (sc; ps; bos; socket) {
+            System.out.println("이름입력 >");
+            String name = sc.nextLine();
+            ps.println(name);
+
+            ps.flush();
+            while (true) {
+                System.out.print(">>:");
+                String input = sc.nextLine();
+                if (input.equals("종료")) {
+                    break;
+                }
+                ps.println(input);
+                ps.flush();
+            }
+
+            System.out.println("종료 !!!!!");
+        }
     }
 }
--- src/ch19/lecture/p01network/C05Server.java
+++ src/ch19/lecture/p01network/C05Server.java
@@ -1,7 +1,42 @@
 package ch19.lecture.p01network;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class C05Server {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws Exception {
+
+        ServerSocket serverSocket = new ServerSocket(9999);
+
+        try (serverSocket) {
+            while (true) {
+                Socket socket = serverSocket.accept();
+                Thread thread = new Thread(() -> {
+                    try {
+                        InputStream is = socket.getInputStream();
+                        InputStreamReader isr = new InputStreamReader(is);
+                        BufferedReader br = new BufferedReader(isr);
+
+                        try (socket; serverSocket; is; isr; br) {
+                            String line = null;
+                            String name = br.readLine();
+
+                            System.out.println(name + "님 접속");
+                            while ((line = br.readLine()) != null) {
+                                System.out.println(name + ">" + line);
+                            }
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+                thread.start();
+            }
+
+        }
     }
 }
